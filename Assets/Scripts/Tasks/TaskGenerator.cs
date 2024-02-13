7b862e19034383d743415d216ec5df0d0f345c25
Clay M - Lab 102 Back Centre <140664778+Plattko@users.noreply.github.com>
2024-02-13 15:02:07 +1100

    Added Task Interval and Task Rate Increase Timers
    - Updated Task Generator script to check if the player has entered the Gameplay state, generate a new task at a slightly randomised task interval, and increase the task rate every 2 minutes.

--- Assets/Scripts/Tasks/TaskGenerator.cs
+++ Assets/Scripts/Tasks/TaskGenerator.cs
@@ -4,24 +4,77 @@ using UnityEngine;
 
 public class TaskGenerator : MonoBehaviour
 {
-    private float firstTaskTime = 5f;
-    private bool firstTaskCompleted = false;
+    private float firstEmailTime = 5f;
+
+    private float taskIntervalTimer;
+    private float taskIntervalTimerReset = 30f;
+
+    private float increaseRateTimer;
+    private float increaseRateTimerReset = 120f;
+
+
+    private enum GameState
+    {
+        Undefined,
+        Onboarding,
+        Gameplay
+    }
     
+    [SerializeField] private GameState gameState;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GiveFirstTask());
+        gameState = GameState.Onboarding;
+        StartCoroutine(GiveFirstEmail());
+
+        taskIntervalTimer = 0f;
+        increaseRateTimer = increaseRateTimerReset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameState == GameState.Gameplay)
+        {
+            // Task interval timer
+            if (taskIntervalTimer > 0f)
+            {
+                taskIntervalTimer -= Time.deltaTime;
+            }
+            else
+            {
+                // Generate new task
+                Debug.Log("Generating new task.");
+
+                // Reset the task interval timer to the new randomised time
+                taskIntervalTimer = taskIntervalTimerReset + Random.Range(-2f, 2f);
+            }
+
+            // Task rate increase timer
+            if (increaseRateTimer > 0f)
+            {
+                increaseRateTimer -= Time.deltaTime;
+            }
+            else
+            {
+                // Increase task rate
+                Debug.Log("Increasing task rate.");
+                
+                if (taskIntervalTimerReset >= 12f)
+                {
+                    taskIntervalTimerReset -= 2f;
+                }
+
+                // Reset the increase rate timer
+                increaseRateTimer = increaseRateTimerReset;
+            }
+        }
     }
 
-    private IEnumerator GiveFirstTask()
+    private IEnumerator GiveFirstEmail()
     {
-        yield return new WaitForSecondsRealtime(firstTaskTime);
+        yield return new WaitForSecondsRealtime(firstEmailTime);
 
     }
 }
